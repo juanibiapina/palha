dace5c1fb527b4d6699aaf021a5e443c3ba6a8d6
Juan Ibiapina <juanibiapina@gmail.com>
2017-08-13 19:38:50 +0200

    refactor: Add type checking with flow
    Closes #6

--- src/index.js
+++ src/index.js
@@ -1,6 +1,32 @@
-export default class App {
-  constructor(parts) {
+// @flow
+
+type UpdateFn<Model, Message> = (Model, Message) => Model;
+type ViewFn<Model, Message, View> = (Model, DispatchFn<Message>) => View;
+
+type RendererFn<View> = (View) => void;
+
+type DispatchFn<Message> = (Message) => void;
+
+type Parts<Model, Message, View> = {
+  initialModel: Model,
+  update: UpdateFn<Model, Message>,
+  view: ViewFn<Model, Message, View>,
+  renderer: RendererFn<View>,
+};
+
+export default class App<Model, Message, View> {
+  currentModel: Model;
+
+  update: UpdateFn<Model, Message>;
+  view: ViewFn<Model, Message, View>;
+
+  renderer: RendererFn<View>;
+
+  dispatch: DispatchFn<Message>;
+
+  constructor(parts: Parts<Model, Message, View>) {
     this.currentModel = parts.initialModel;
+
     this.view = parts.view;
     this.renderer = parts.renderer;
     this.update = parts.update;
@@ -12,11 +38,11 @@ export default class App {
     this.renderer(this.view(this.currentModel, this.dispatch));
   }
 
-  performUpdate(message) {
+  performUpdate(message: Message) {
     this.currentModel = this.update(this.currentModel, message);
   }
 
-  dispatch(message) {
+  dispatch(message: Message) {
     this.performUpdate(message);
     this.render();
   }
