56b692274f520a707ef9e2d40105a89b712e1dbb
Juan Ibiapina <juanibiapina@gmail.com>
2017-10-21 22:55:10 +0200

    feature: Remove flow

--- src/index.js
+++ src/index.js
@@ -1,30 +1,5 @@
-// @flow
-
-export type UpdateFn<Model, Message> = (Model, Message) => Model;
-export type ViewFn<Model, Message, View> = (Model, DispatchFn<Message>) => View;
-
-export type RendererFn<View> = (View) => void;
-
-export type DispatchFn<Message> = (Message) => void;
-
-export type Parts<Model, Message, View> = {
-  initialModel: Model,
-  update: UpdateFn<Model, Message>,
-  view: ViewFn<Model, Message, View>,
-  renderer: RendererFn<View>,
-};
-
-export default class App<Model, Message, View> {
-  currentModel: Model;
-
-  update: UpdateFn<Model, Message>;
-  view: ViewFn<Model, Message, View>;
-
-  renderer: RendererFn<View>;
-
-  dispatch: DispatchFn<Message>;
-
-  constructor(parts: Parts<Model, Message, View>) {
+export default class App {
+  constructor(parts) {
     this.currentModel = parts.initialModel;
 
     this.view = parts.view;
@@ -38,11 +13,11 @@ export default class App<Model, Message, View> {
     this.renderer(this.view(this.currentModel, this.dispatch));
   }
 
-  performUpdate(message: Message) {
+  performUpdate(message) {
     this.currentModel = this.update(this.currentModel, message);
   }
 
-  dispatch(message: Message) {
+  dispatch(message) {
     this.performUpdate(message);
     this.render();
   }
